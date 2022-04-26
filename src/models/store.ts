61a961213bd94f0a8b27bc512b09148ccbaf642e
Helton Silveira <helton@heltonsouza.com.br>
2022-04-26 11:55:16 -0300

    feat: add store class

---
+++ src/models/store.ts
@@ -0,0 +1,13 @@
+import { Subscription } from 'rxjs';
+import { Action } from './../interfaces/action';
+import { BehaviorSubject } from 'rxjs';
+
+export class Store<T> {
+  private state: BehaviorSubject<T>;
+  private reducer: (state: T, action: Action) => T;
+
+  constructor(initialState: T, reducer: (state: T, action: Action) => T) {
+    this.state = new BehaviorSubject(initialState);
+    this.reducer = reducer
+  }
+}

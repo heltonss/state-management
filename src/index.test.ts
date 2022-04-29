7446b2ccc1aa9a68527a169f7f1480b65117fe1e
Helton Silveira <helton@heltonsouza.com.br>
2022-04-29 15:33:15 -0300

    feat: add app for example

--- src/index.test.ts
+++
@@ -1,5 +0,0 @@
-it('should behave...', () => {
-    const x: number = 4;
-    const y: number = 5;
-    expect(x + y).toBe(9)
-});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("hello world")
+import { Action } from './interfaces/action';
+import { Store } from './lib/store';
+
+interface State {
+  points: number;
+  team: string;
+}
+
+const reducer = (state:State, action:Action):State => {
+  switch (action.type) {
+    case "CHANGE_TEAM": {
+      return {...state, team: action.payload}
+    }
+    case "INCREMENT": {
+      return {...state, points: state.points + action.payload}
+    }
+    default:
+      return state
+  }
+}
+
+const initialState: State = {
+  team: "",
+  points: 0
+}
+
+const store = new Store(initialState, reducer);
+store.select("team").subscribe(name => console.log("team TEAMd changed",  name));
+store.select("points").subscribe(points => console.log("team TEAMd changed",  points));
+
+store.asyncDispatch("CHANGE_TEAM", () => new Promise(resolve => setTimeout(() => resolve(2),50000)))
+store.dispatch({ type: "CHANGE_TEAM", payload: "First TEAM" });
+store.dispatch({ type: "INCREMENT", payload: 5 });
+store.dispatch({ type: "CHANGE_TEAM", payload: "Second TEAM" });
+store.dispatch({ type: "INCREMENT", payload: 3 });
